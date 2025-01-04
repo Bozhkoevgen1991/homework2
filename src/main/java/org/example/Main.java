261bb81c87fbdcaac480d801675645876d1ce285
Evgeniy Bozko <drakon-rulit@mail.ru>
2025-01-04 19:12:33 +0300

    Исправил замечания Алексея

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Домашка Евгени номер 1");
+        System.out.println("Доработка домашки, после замечания Алексея");
     }
 }
